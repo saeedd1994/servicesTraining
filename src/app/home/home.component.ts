5f3ba853cdebf5b806632ff4e734a940e5edd8fd
saeed.da1994 <saeed.da1994@gmail.com>
2023-12-07 00:40:04 +0330

    import mat tab a add component selector in each tab

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatTabsModule} from "@angular/material/tabs";
+import {FristCompComponent} from "../frist-comp/frist-comp.component";
+import {SecondCompComponent} from "../second-comp/second-comp.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTabsModule,
+    FristCompComponent,
+    SecondCompComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
